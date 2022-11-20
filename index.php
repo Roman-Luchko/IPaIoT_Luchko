b6a5dd8ddc1502f4885efb6f3b1ca1d09cdfb466
Roman-Luchko <102596160+Roman-Luchko@users.noreply.github.com>
2022-11-20 15:43:02 +0100

    Update index.php

--- index.php
+++ index.php
@@ -13,13 +13,22 @@
 		// WARNING: this doesn't include sanitization
 		// and validation
 		//---------------------------------------------
-		if (isset($_POST['name'], $_POST['email'])) {
-			$name = $_POST['name'];
-			$email = $_POST['email'];
+		if (isset($_POST['first'], $_POST['second']),$_POST['third']),$_POST['fourth']),$_POST['fifth']),$_POST['sixth']),) {
+			$first = $_POST['first'];
+			$second = $_POST['second'];
+			$third = $_POST['third'];
+			$fourth = $_POST['fourth'];
+			$fifth = $_POST['fifth'];
+			$sixth = $_POST['sixth'];
 
 			// show the $name and $email
-			echo "Thanks $name for your subscription.<br>";
-			echo "Please confirm it in your inbox of the email $email.";
+			echo "first $first<br>";
+			echo "second $second<br>";
+			echo "third $third<br>";
+			echo "fourth $fourth<br>";
+			echo "fifth $fifth<br>";
+			echo "sixth $sixth<br>";
+			
 		} else {
 			echo 'You need to provide your name and email address.';
 		}
@@ -28,14 +37,33 @@
         <form action="" method="post">
             <div>
                 <label for="name">Name:</label>
-                <input type="text" name="name" required="required" placeholder="Enter your name" />
+                <input type="text" name="name" required="required" placeholder="Enter first" />
             </div>
 
             <div>
-                <label for="name">Email:</label>
-                <input type="email" name="email" required="required" placeholder="Enter your email" />
+                <label for="name">Name:</label>
+                <input type="text" name="name" required="required" placeholder="Enter second" />
             </div>
-
+		
+	    <div>
+                <label for="name">Name:</label>
+                <input type="text" name="name" required="required" placeholder="Enter third" />
+            </div>
+		
+	    <div>
+                <label for="name">Name:</label>
+                <input type="text" name="name" required="required" placeholder="Enter fourth" />
+            </div>
+	    
+	    <div>
+                <label for="name">Name:</label>
+                <input type="text" name="name" required="required" placeholder="Enter fifth" />
+            </div>
+	    
+	    <div>
+                <label for="name">Name:</label>
+                <input type="text" name="name" required="required" placeholder="Enter sixth" />
+            </div>
             <button type="submit">Subscribe</button>
         </form>
     </main>
