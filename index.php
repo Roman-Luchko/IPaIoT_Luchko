7e638e8ba7c09c38ee747ab626d6767d70d0cf04
Roman-Luchko <102596160+Roman-Luchko@users.noreply.github.com>
2022-11-20 13:37:54 +0100

    Update index.php

--- index.php
+++ index.php
@@ -10,13 +10,20 @@
         <form method="POST" action="index.php">
             <input type="text" name="first" placeholder="Enter first num">
             <input type="text" name="second" placeholder="Enter second num">
+            <input type="text" name="third" placeholder="Enter third num">
+            <input type="text" name="fourth" placeholder="Enter fourth num">
+            <input type="text" name="fifth" placeholder="Enter fifth num">
+            <input type="text" name="sixth" placeholder="Enter sixth num">
             <button type="submit" >Send information</button>
-
         </form>
-
     </body>
     </html>
 <?php
-echo (int)$_POST['first']."<br>";
-echo (int)$_POST['second'];
+$f = fopen('result.txt', 'r');
+fwrite($_POST['first']);
+fwrite($_POST['second']);
+fwrite($_POST['third']);
+fwrite($_POST['fourth']);
+fwrite($_POST['fifth']);
+fwrite($_POST['sixth']);
 ?>
