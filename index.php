246b51d8c3d7360258af1031f67e5bc1f00ee77b
Roman-Luchko <102596160+Roman-Luchko@users.noreply.github.com>
2022-11-20 13:25:43 +0100

    Update index.php

--- index.php
+++ index.php
@@ -1,15 +1,22 @@
-<html>
-<head>
-<title>Пример веб-страницы</title>
-</head>
-<body>
-  <?
-echo "<html><body><h1>";
-
-echo " Hello, $my_name";
-
-echo "</h1></body></html>";
-
+<?php
+echo (int)$_POST['first']."<br>";
+echo (int)$_POST['second'];
 ?>
- </body>
-</html>
+
+    <!DOCTYPE html>
+    <html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <title>Qwr</title>
+    </head>
+    <body>
+        <form method="POST" action="index.php">
+ // или action='', или вообще без action 
+            <input type="text" name="first" placeholder="Enter first num">
+            <input type="text" name="second" placeholder="Enter second num">
+            <button type="submit" >Send information</button>
+
+        </form>
+
+    </body>
+    </html>
