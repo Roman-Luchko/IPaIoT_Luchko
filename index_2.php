4ce0f06e5a899cec49ef4c6b502cede97c16d93e
Roman-Luchko <102596160+Roman-Luchko@users.noreply.github.com>
2022-11-20 16:16:31 +0100

    Update index_2.php

--- index_2.php
+++ index_2.php
@@ -1,8 +1,6 @@
 <?php
-$fd = fopen("hello.txt", 'a+') or die("не удалось создать файл");
+$fd = fopen("hello.txt", 'w') or die("не удалось создать файл");
 $str = "Привет мир";
 fwrite($fd, $str);
 fclose($fd);
 ?>
-
-
