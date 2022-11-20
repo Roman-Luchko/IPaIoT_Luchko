5ab2191d2ac6a85b2ca98984d866bdf38f4eda4a
Roman-Luchko <102596160+Roman-Luchko@users.noreply.github.com>
2022-11-20 15:18:13 +0100

    Update index.php

--- index.php
+++ index.php
@@ -7,6 +7,16 @@
         <title>Qwr</title>
     </head>
     <body>
+     <?php
+        $f = fopen('result.txt', 'w+');
+        $first = $_POST['first']; 
+        $second = $_POST['second']; 
+        fwrite($f,$first);
+        fwrite($f,$second);
+        echo "first $first";
+        echo "second $second";
+        fclose($f);
+    ?>
         <form action="">
             <input type = "text" name="first" value = "first" />
             <input type = "text" name="second" value = "second" />
@@ -14,13 +24,4 @@
         </form>
     </body>
     </html>
-<?php
-$f = fopen('result.txt', 'w+');
-$first = $_POST['first']; 
-$second = $_POST['second']; 
-fwrite($f,$first);
-fwrite($f,$second);
-echo "first $first";
-echo "second $second";
-fclose($f);
-?>
+
