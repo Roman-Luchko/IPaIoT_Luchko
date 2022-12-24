5f0dd1e0d630dfb68a158a4e89451b5655560111
Roman-Luchko <102596160+Roman-Luchko@users.noreply.github.com>
2022-12-24 18:10:16 +0100

    Update index.php

--- index.php
+++ index.php
@@ -1,14 +1,9 @@
+<html>
+<body>
 <?php
-echo '<h1>index page</h1>';
-
-$sn1 = $_GET["a"];
-$sn2 = $_GET["b"];
-
-$text = "a=" . $sn1 . " b=" . $sn2;
-$sum = $sn1 + $sn2;
-
-echo $text;
-echo "<br>";
-echo "Sum:" . $sum;
-
+echo “Helloooooo”;
+$names = $_POST[“names”]; 
+echo $names;
 ?>
+</body>
+</html>
