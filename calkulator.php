6e548ab12fe651f0123185ddc973f7a1434f9d9a
Roman-Luchko <102596160+Roman-Luchko@users.noreply.github.com>
2022-12-25 23:37:42 +0100

    Update calkulator.php

--- calkulator.php
+++ calkulator.php
@@ -1,8 +1,23 @@
+//сам калькулятор
 <?php
-echo 'Hellooooooo ';
-$user = $_POST["user"]; 
-$password = $_POST["password"]; 
-echo $user;
+  $number1 = $_POST['number1'];
+  $number2 = $_POST['number2'];
+  $znak = $_POST['znak'];
+  $result = null;
+  if(isset($number1) and isset($number2) and isset($znak)){
+    switch($znak){
+      case '+':
+        $result = $number1 + $number2;
+        break;
+      case = '-':
+        $result = $number1 - $number2;
+        break;
+      case = '/':
+        $result = $number1 / $number2;
+        break;
+      case = '*':
+        $result = $number1 * $number2;
+        break;      
+    }
+  }
 ?>
-</body>
-</html>
