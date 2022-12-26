387a2f8c00f91e3e99b7079c99dd550e02c14587
Roman-Luchko <102596160+Roman-Luchko@users.noreply.github.com>
2022-12-26 20:10:52 +0100

    Update calkulator.php

--- calkulator.php
+++ calkulator.php
@@ -1,7 +1,7 @@
 <?php
 
 
-$connect = mysqli_connect('mysqlluchko.mysql.database.azure.com','rl718un','Uj*hm9e2','testdatabase');
+$connect = new mysqli('mysqlluchko.mysql.database.azure.com','rl718un','Uj*hm9e2','testdatabase');
 echo 'ty durak!!!';
 ?>
 
