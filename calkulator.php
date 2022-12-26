e5042e21bf0e18e22165fae97140474037bd8631
Roman-Luchko <102596160+Roman-Luchko@users.noreply.github.com>
2022-12-26 22:34:27 +0100

    Update calkulator.php

--- calkulator.php
+++ calkulator.php
@@ -1,11 +1,11 @@
 <?php
-$file = fopen('https://github.com/Roman-Luchko/IPaIoT_Luchko/blob/9e42a85e14bad77db15d82137ad2826f8a78a71c/text.txt',"a+");
+$file = fopen('text.txt',"a+");
 fwrite($file,"Hello Worl\r\n");
 fclose($file);
-$f = fopen('https://github.com/Roman-Luchko/IPaIoT_Luchko/blob/9e42a85e14bad77db15d82137ad2826f8a78a71c/text.txt',"r");
+$f = fopen('text.txt',"r");
 $ftext = fgets($f, 999);
 echo $ftext."";
 echo 'hhh';
-echo file_get_contents("https://github.com/Roman-Luchko/IPaIoT_Luchko/blob/9e42a85e14bad77db15d82137ad2826f8a78a71c/text.txt");
+echo file_get_contents("text.txt");
 echo 'ddd';
 ?>
