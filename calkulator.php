e1e89df8857e42a91cda8d56f5a56680e72a35f2
Roman-Luchko <102596160+Roman-Luchko@users.noreply.github.com>
2022-12-26 22:41:36 +0100

    Update calkulator.php

--- calkulator.php
+++ calkulator.php
@@ -1,11 +1,31 @@
 <?php
+
+$pocet = 0;
+
 $file = fopen('text.txt',"a+");
 fwrite($file,"Hello Worl\r\n");
 fclose($file);
+
 $f = fopen('text.txt',"r");
 $ftext = fgets($f, 999);
+
 echo $ftext."";
 echo 'hhh';
 echo file_get_contents("text.txt");
 echo 'ddd';
+fclose($f);
+
+if(pocet == 10){
+  $file = fopen('text.txt',"w+");
+  fclose($file);
+}
+pocet++;
 ?>
+<html>
+  <head>
+    <title>LoT ESP</title>
+  </head>
+  <body>
+    <p>text</p>
+  </body>
+</html>
