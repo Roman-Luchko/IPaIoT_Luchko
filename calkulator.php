2355a69e11a7f0a84ab1a266b280ee0f164a5c83
Roman-Luchko <102596160+Roman-Luchko@users.noreply.github.com>
2022-12-26 21:03:59 +0100

    Update calkulator.php

--- calkulator.php
+++ calkulator.php
@@ -1,7 +1,7 @@
 <?php
 
 
-$connect = new MySQLi('mysqlluchko.mysql.database.azure.com','rl718un','Uj*hm9e2','testdatabase') or die("Unable connect");
+$connect = new MySQLi('mysqlluchko.mysql.database.azure.com','rl718un','Uj*hm9e2') or die("Unable connect");
 echo 'ty durak!!!';
 ?>
 
