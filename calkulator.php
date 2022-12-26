eb953a169214661b5b64d3db907c70a9aad76371
Roman-Luchko <102596160+Roman-Luchko@users.noreply.github.com>
2022-12-26 21:53:32 +0100

    Update calkulator.php

--- calkulator.php
+++ calkulator.php
@@ -1,20 +1,4 @@
 <?php
-$host = 'mysqlluchko.mysql.database.azure.com';
-$username = 'rl718un';
-$password = 'Uj*hm9e2';
-$db_name = 'testdatabase';
-
-//Initializes MySQLi
-$conn = mysqli_init();
-
-mysqli_ssl_set($conn,NULL,NULL, "/var/www/html/DigiCertGlobalRootG2.crt.pem", NULL, NULL);
-
-mysqli_real_connect($conn, 'mysqlluchko.mysql.database.azure.com', 'rl718un', 'yourpassword', 'Uj*hm9e2', 3306, NULL, MYSQLI_CLIENT_SSL);
-
-//If connection failed, show the error
-if (mysqli_connect_errno())
-{
-    die('Failed to connect to MySQL: '.mysqli_connect_error());
-}
-?>
+$file = fopen('text.txt',w+);
+fwrite($file,"Hello World\r\n");
 echo 'hhh';
