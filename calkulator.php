7a1140773f6408e785d2533e5260ae45b486f0a1
Roman-Luchko <102596160+Roman-Luchko@users.noreply.github.com>
2022-12-28 00:08:04 +0100

    Update calkulator.php

--- calkulator.php
+++ calkulator.php
@@ -2,8 +2,11 @@
 $text = fopen("text.txt",'r');
 $result = fgets($text);
 $result = explode(" ",$result);
+$pocet = 0;
 foreach($result as $i){
   echo $i;
+  echo pocet;
+  pocet += 1;
 }
 fclose($text);
 ?>
