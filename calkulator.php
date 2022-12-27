ade4b5696ebfd26fabb90a16ee1b0d89bee05415
Roman-Luchko <102596160+Roman-Luchko@users.noreply.github.com>
2022-12-27 23:54:44 +0100

    Update calkulator.php

--- calkulator.php
+++ calkulator.php
@@ -1,13 +1,13 @@
 <?php
 $text = fopen("text.txt","w");
-fwrite($text,"ggg\n");
+fwrite($text,"ggg\n tt i");
 fwrite($text,"ddd\n");
 fclose($text);
 
 $text = fopen("text.txt",'r');
 $result = fgets($text);
-$result = fgets($text);
-echo $result;
+$result = explode(" ",$result);
+print_r($result);
 fclose($text);
 ?>
 
