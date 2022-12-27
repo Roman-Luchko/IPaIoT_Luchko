1a144188e830b17b85b7e47e35093561d5f89058
Roman-Luchko <102596160+Roman-Luchko@users.noreply.github.com>
2022-12-28 00:17:50 +0100

    Update calkulator.php

--- calkulator.php
+++ calkulator.php
@@ -1,8 +1,11 @@
 <?php
+
+$text = fopen("text.txt",'w');
+fwrite("ggg ttt 5");
+fclose($text);
 $text = fopen("text.txt",'r');
 $result = fgets($text);
 $result = explode(" ",$result);
-echo result[0];
 $pocet = 0;
 foreach($result as $i){
   echo $i;
