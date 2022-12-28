fd2fdd7ce869072222f2b41a0b1e9508d55609a9
Roman-Luchko <102596160+Roman-Luchko@users.noreply.github.com>
2022-12-28 22:41:12 +0100

    Update index.php

--- index.php
+++ index.php
@@ -5,7 +5,11 @@
                     </head>
                     <body>                              
                               <table background = "yellow">
-                                        // -> riadok 1
+                                        
+                                        //<-_->//                                     
+                                        // riadok 1
+                                        //<-_->//
+                                        
                                         <?php $text = file("text.txt"); ?>
                                         <tr>
                                                   <th>Temperature</th>
@@ -42,6 +46,48 @@
                                                        ?>
                                                   </td>
                                         </tr>
+                                        
+                                        //<-_->//                                     
+                                        // riadok 2
+                                        //<-_->//
+                                                                             
+                                        <tr>
+                                                  <th>Temperature</th>
+                                                  <th>Humidity</th>
+                                                  <th>Svetlo</th>
+                                                  <th>Voda</th>
+                                        </tr>  
+                                        <?php
+                                                  $res = $text[1]; 
+                                                  $result = explode(" ", $res);
+                                                  print_r($result);
+                                        ?>          
+                                        <tr>
+                                                  <td><?php echo $result[1]; ?></td>
+                                                  <td><?php echo $result[3]; ?></td>
+                                                  <td>
+                                                       <?php 
+                                                            if($result[5] == 1){
+                                                                      echo "+";
+                                                            }
+                                                            else{
+                                                                      echo "-";
+                                                            }
+                                                       ?>
+                                                  </td>
+                                                  <td>
+                                                       <?php 
+                                                            if($result[7] == 0){
+                                                                      echo "-";
+                                                            }
+                                                            else{
+                                                                      echo "+";
+                                                            }
+                                                       ?>
+                                                  </td>
+                                        </tr>
+                                        
+                                        
                               </table>
                     </body>
                                        
