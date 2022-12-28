4391f03c78232d3259ea31fdba1c08f1220470ab
Roman-Luchko <102596160+Roman-Luchko@users.noreply.github.com>
2022-12-28 02:29:54 +0100

    Update index.php

--- index.php
+++ index.php
@@ -5,9 +5,17 @@ $temperature = $_POST['temperature'];
 $humidity = $_POST['humidity'];
 $svetlo = $_POST['svetlo'];
 $voda = $_POST['voda'];
+
+// -> разовая запись в файл
+$text = fopen("text.txt","w+");
 $result = "temperature-> ".$temperature.
           " humidity-> ".$humidity.
           " svetlo-> ".$svetlo.
           " voda-> ".$voda; 
-echo $result;
+fwrite($result);
+$res = gets($result);
+fclose($text);
+
+echo $res;
+
 ?>
