8e777f30ffae2dab176c7b3a378208b665a303d2
Roman-Luchko <102596160+Roman-Luchko@users.noreply.github.com>
2022-12-28 02:54:08 +0100

    Update index.php

--- index.php
+++ index.php
@@ -1,10 +1,10 @@
 <?php
 
 //-> создаю переменные, которые принимаю с esp
-$temperature = $_POST['temperature'];
-$humidity = $_POST['humidity'];
-$svetlo = $_POST['svetlo'];
-$voda = $_POST['voda'];
+$temperature = $_GET['temperature'];
+$humidity = $_GET['humidity'];
+$svetlo = $_GEt['svetlo'];
+$voda = $_GET['voda'];
 
 // -> разовая запись в файл
 $text = fopen("text.txt","w");
