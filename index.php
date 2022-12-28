9d71cec192b3b7c237e350968773f777fed5cf11
Roman-Luchko <102596160+Roman-Luchko@users.noreply.github.com>
2022-12-28 20:20:56 +0100

    Update index.php

--- index.php
+++ index.php
@@ -42,7 +42,7 @@ elseif($pocet == 12){
                $text[7] =  $text[6]; 
                $text[6] =  $text[5]; 
                $text[5] =  $text[4]; 
-               $text[3] =  $text[3]; 
+               $text[4] =  $text[3]; 
                $text[3] =  $text[2]; 
                $text[2] =  $text[1]; 
                $text[1] =  $text[0]; 
