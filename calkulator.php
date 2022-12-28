8faee6dec1923c71f9400a172aada79b1a23f23f
Roman-Luchko <102596160+Roman-Luchko@users.noreply.github.com>
2022-12-28 01:19:26 +0100

    Update calkulator.php

--- calkulator.php
+++ calkulator.php
@@ -1,11 +1,12 @@
 <?php
 
-$text = fopen("text.txt",'w');
-fwrite($text,"ggg ttt 5");
-fclose($text);
-$text = fopen("text.txt",'r');
-$result = fgets($text);
-$result = explode(" ",$result);
-echo $result[0];
-fclose($text);
+//-> создаю переменные, которые принимаю с esp
+$temperature = GET['temperature'];
+$humidity = GET['humidity'];
+$svetlo = GET['svetlo'];
+$voda = GET['voda'];
+$result = "temperature-> ".$temperature.
+          " humidity-> ".$humidity.
+          " svetlo-> ".$svetlo.
+          " voda->".$voda; 
 ?>
