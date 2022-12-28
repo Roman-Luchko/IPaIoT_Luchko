00a4aeff7761ef3e596fd031b0efa623debd7c03
Roman-Luchko <102596160+Roman-Luchko@users.noreply.github.com>
2022-12-28 18:42:15 +0100

    Update index.php

--- index.php
+++ index.php
@@ -11,11 +11,11 @@ while(($buffer = fgets($text)) !== false){
            $pocet++;        
 }
 echo $pocet++;
-if($pocet > 12){
+if($pocet > 13){
           $text = fopen("text.txt","w");
           fclose($text);
 }
-elseif($pocet < 12){
+elseif($pocet < 13){
           // -> запись в файл
           if(isset($temperature)){           
                     $text = fopen("text.txt","a");
