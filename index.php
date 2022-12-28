f3bd96827892538cec32fbb6ec5af07807cdf1d6
Roman-Luchko <102596160+Roman-Luchko@users.noreply.github.com>
2022-12-28 14:25:33 +0100

    Update index.php

--- index.php
+++ index.php
@@ -5,12 +5,12 @@ $temperature = $_GET['temperature'];
 $humidity = $_GET['humidity'];
 $svetlo = $_GET['svetlo'];
 $voda = $_GET['voda'];
-$pocet = 0;
+$pocet = $_GET['pocet];
 if($pocet == 12){
           $text = fopen("text.txt","w");   
           fclose($text);
 }
-// -> разовая запись в файл
+// -> запись в файл
 $text = fopen("text.txt","a");
 $result = "temperature-> ".$temperature.
           " humidity-> ".$humidity.
