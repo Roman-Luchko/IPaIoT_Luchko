d746a2bc9c2db78f3e65b32283b52dc0abbe7464
Roman-Luchko <102596160+Roman-Luchko@users.noreply.github.com>
2023-01-19 00:52:13 +0100

    Update index.php

--- index.php
+++ index.php
@@ -18,7 +18,7 @@
         $result = $sn1 * $sn2;
         }elseif($operacia == "+"){
         $result = $sn1 + $sn2;
-        elseif($operacia == "/"){
+        }elseif($operacia == "/"){
         $result = $sn1 / $sn2;
         }elseif($operacia == "-"){
         $result = $sn1 - $sn2;
