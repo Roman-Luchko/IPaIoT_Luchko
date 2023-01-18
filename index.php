1061ea4f5fdbbe5f4e9bae9bfcf6551b0502087c
Roman-Luchko <102596160+Roman-Luchko@users.noreply.github.com>
2023-01-19 00:08:20 +0100

    Update index.php

--- index.php
+++ index.php
@@ -2,16 +2,16 @@
   $a = $_GET['a'];
   $znak = $_GET['znak'];
   $c = $_GET['c'];
-  if($znak == "-"){
+  if($znak == '-'){
     $result = $a-$b;
   }
-  elseif($znak == "+"){
+  elseif($znak == '+'){
     $result = $a+$b;
   }
-  elseif($znak == "*"){
+  elseif($znak == '*'){
     $result = $a*$b;
   }
-  elseif($znak == "/"){
+  elseif($znak == '/'){
      $result = $a*$b;
   }
   echo "Result je ".$result;
