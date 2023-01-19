d614348f3c8340732958cc52a8720824aa35c582
Roman-Luchko <102596160+Roman-Luchko@users.noreply.github.com>
2023-01-19 18:15:36 +0100

    Update skuska.php

--- skuska.php
+++ skuska.php
@@ -4,13 +4,13 @@ if (isset($_POST['but'])){
   if (isset($_COOKIE['count'])){
     $num = $_COOKIE['count'] + 1;
   }
-  else if(isset($_POST['reset'])){
+}else if(isset($_POST['reset'])){
       if(isset($_COOKIE['count'])){
           $num = 0;
       }
   }
   setcookie('count', $num);
-}
+
 echo 'Вы нажали на кнопку '.$num. ' раз';
 
 ?>
