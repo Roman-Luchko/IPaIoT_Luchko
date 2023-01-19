592b9d413396985f0f8d9491aa362eb058a026e6
Roman-Luchko <102596160+Roman-Luchko@users.noreply.github.com>
2023-01-19 15:25:34 +0100

    Update priklad.php

--- priklad.php
+++ priklad.php
@@ -24,7 +24,7 @@ fclose($text);
 
 ?>
 <html>
-  <form action = "priklad.php" method="post">
+  <form action = "" method="post">
         <input type="button" name="pocet",value="pocet"><br>
         <input type="button" name="reset",value="reset"><br>
         <input type="submit" value="Submit">
