63cceac6915469e3560130d6495e2b5dfae92fb0
Roman-Luchko <102596160+Roman-Luchko@users.noreply.github.com>
2023-01-19 01:49:16 +0100

    Update index.php

--- index.php
+++ index.php
@@ -1,33 +1,4 @@
 <?php
-        echo '<h1>index2.php web page with parameters</h1>';
-
-        echo "uds                ";
-
-        echo "try calcul";
-        echo "<br>";
-        $sn1 = $_GET["a"];
-        $sn2 = $_GET["b"];
-
-        $text = "a=" . $sn1 . " b=" . $sn2;
-        $sum = $sn1 + $sn2;
-
-        $operacia = $_GET["operation"];
-        $text = "a=" . $sn1 . " b=" . $sn2 . $operacia . "opartion? ";
-        echo $text;
-        if($operacia == "*"){
-        $result = $sn1 * $sn2;
-        }
-        if($operacia == "-"){
-        $result = $sn1 - $sn2;
-        }
-        if($operacia == "/"){
-        $result = $sn1 / $sn2;
-        }
-        if($operacia == "+"){
-        $result = $sn1 + $sn2;
-        }else 
-        $result = 0;
-        echo "<br>";
-        echo $operacia;
-        echo "Result:" . $result;
-    ?>
+$a = $_GET["vlhkost"];
+echo file_put_contents("test12.txt",$a);
+?>
