5d6c9031099a1b90b93ce9b76e2bf7b5979abfba
Roman-Luchko <102596160+Roman-Luchko@users.noreply.github.com>
2023-01-19 02:45:12 +0100

    Update index.php

--- index.php
+++ index.php
@@ -1,10 +1,4 @@
 <?php
-$b = $_GET["vlhkost"];
-$file = fopen("tester.txt", "w");
-fwrite($file,$b);
-fclose($file);
-$file = fopen("tester.txt", "r");
-$a=fread($file,filesize("tester.txt"));
-fclose($file);
-echo "a:".$a;
+
+echo "a:";
 ?>
