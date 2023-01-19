04cbb51f5f6db7f2ac9c1b894d67d93c468eb749
Roman-Luchko <102596160+Roman-Luchko@users.noreply.github.com>
2023-01-19 18:12:35 +0100

    Update skuska.php

--- skuska.php
+++ skuska.php
@@ -4,11 +4,16 @@ if (isset($_POST['but'])){
   if (isset($_COOKIE['count'])){
     $num = $_COOKIE['count'] + 1;
   }
+  else if(isset($_POST['reset'])){
+      if(isset($_COOKIE['count'])){
+          $num = 0;
+      }
+  }
   setcookie('count', $num);
 }
 echo 'Вы нажали на кнопку '.$num. ' раз';
 
 ?>
-<form action="skuska.php" method="post">
+<form action="registration.php" method="post">
     <input type="submit" name="but" value="<?php echo $num;?>">
 </form>
