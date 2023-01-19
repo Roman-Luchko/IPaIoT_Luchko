6457c9ca01cd192da607f2c076c8cca510cea52e
Roman-Luchko <102596160+Roman-Luchko@users.noreply.github.com>
2023-01-19 13:59:27 +0100

    Update skuska.php

--- skuska.php
+++ skuska.php
@@ -1,6 +1,30 @@
 <?php
-//создать переменные для кнопок
-$pocet = $_GET["pocet"];
-$reset = $_GET["reset];
-//сперва считать с файла предыдущие количества итераций
 
+//создать переменные для кнопок
+$pocet_button = $_POST["pocet"];
+$reset_button = $_POST["reset];
+
+//сперва считать с файла предыдущие количества итераций
+$file = fopen("g.txt","w+");
+
+//записать их в строку
+$stroka = fgets($file);
+
+//разделить строку на масив и дать значения(их 2) до отдельной переменной
+$v = explode($stroka);
+$pocet = $v[0] + $pocet_button;
+$reset = $v[1] + $reset_button;
+echo "Pocet -> ".$pocet;
+echo "Reset -> ".$reset;
+
+//записать нажатие опять до файла
+$text = $pocet." ".$reset." "."\n";
+fwrite($text);
+fclose($text);
+
+?>
+<form>
+  <input type="text" name="pocet",value="pocet"><br>
+  <input type="text" name="reset",value="reset"><br>
+  <input type="submit" value="Submit">
+</form>
