f16de33b2ad340f87db63ff01b6983cd890edd64
Roman-Luchko <102596160+Roman-Luchko@users.noreply.github.com>
2023-01-19 16:03:14 +0100

    Update priklad.php

--- priklad.php
+++ priklad.php
@@ -1,32 +1,15 @@
 <?php
 
 //создать переменные для кнопок
-$pocet_button = $_POST["pocet"];
-$reset_button = $_POST["reset"];
+//$button_1 = $_POST["button_1"];
+//$button_2 = $_POST["button_2"];
 
-//сперва считать с файла предыдущие количества итераций
-$file = fopen("g.txt","w+");
-
-//записать их в строку
-$stroka = fgets($file);
-
-//разделить строку на масив и дать значения(их 2) до отдельной переменной
-$v = explode($stroka);
-$pocet = $v[0] + $pocet_button;
-$reset = $v[1] + $reset_button;
-echo "Pocet -> ".$pocet;
-echo "Reset -> ".$reset;
-
-//записать нажатие опять до файла
-$text = $pocet." ".$reset." "."\n";
-fwrite($text);
-fclose($text);
-
+if(isset($_POST['button'])){
+	echo 'button_1 was clicked';
+}
 ?>
 <html>
-  <form method="post">
-        <input type="button" name="pocet",value="pocet"><br>
-        <input type="button" name="reset",value="reset"><br>
-        <input type="submit" value="Submit">
-  </form>
+  	<form method="post">
+  		<input type="submit" name="button"value="button" />  		
+	</form>
 </html>
