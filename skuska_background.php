1404c5655b5ec0c7d728d3e35302bd73b67c2ea4
Roman-Luchko <102596160+Roman-Luchko@users.noreply.github.com>
2023-01-26 09:02:57 +0100

    Create skuska_background.php

---
+++ skuska_background.php
@@ -0,0 +1,13 @@
+<?php
+  $teplota = $_GET['teplota'];
+  
+  $file = fopen("text.txt","w");
+
+  echo "teplota je ->".teplota;  
+
+  if(isset($teplota)){
+    $file = fwrite($file,$teplota);
+  }
+
+  fclose($file);
+?>
